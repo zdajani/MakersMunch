6e979e98d5b2d503d26e93876f3864d18e8959ef
Paul Harker <harkerp@gmail.com>
2015-07-14 17:01:30 +0100

    one issue solved

--- app/public/googleApi.js
+++ app/public/googleApi.js
@@ -8,7 +8,7 @@ function getApiDetails(placeId) {
   map = new google.maps.Map(document.getElementById('map'), {
      center: makers,
      zoom: 15
-   });  
+   });
   var request = {
     placeId: placeId
   };
@@ -18,14 +18,15 @@ function getApiDetails(placeId) {
     console.log(results);
     html += '<div id="location">';
     html += "<div id='placeName'>" + results.name + "</div>";
-    if(results.photos[0]) {
+    if(results.photos) {
     photoUrl = results.photos[0].getUrl({maxWidth:1000, maxHeight: 1000});
     html += "<div id='photo'><img src='" + photoUrl + "' width = '200px' height = '200px'></div>";
   }
-    html += "<div id=writing>" + results.address_components[0].short_name +
+    html += "<div id=writing>"
+                               + results.address_components[0].short_name +
                            " " + results.address_components[1].short_name +
                            " " + results.address_components[2].short_name + "</br>"
-                               + results.address_components[6].short_name + "</div>";
+                            if (results.address_components[6]) { + results.address_components[6].short_name } + "</div>";
     html += "<div>" + results.formatted_phone_number + "</div>";
     html += "<div> Is it open right now?"
     if (results.opening_hours.open_now == true) {
